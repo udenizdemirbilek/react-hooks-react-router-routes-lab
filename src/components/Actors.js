e32bb08bc8784e86d6dc30d0e4a8e11e77712166
demirbileku <udemirbilek2@gmail.com>
2021-06-23 18:41:05 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsDiv = actors.map((actor, index) => {
+    return (
+      <div key= {index}>
+        <h3>Name: {actor.name}</h3>
+        <p>Movies: </p>
+        <ul>
+          {actor.movies.map((movie,index) => <li key={index}> {movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorsDiv}
+    </div>)
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorDiv = directors.map((director, index) => {
+    return (
+      <div key={index}>
+        <h3>Name: {director.name}</h3>
+        <p>Movies: </p>
+        <ul>{director.movies.map((movie,index) => <li key={index}> {movie}</li>)}</ul>
+      </div>
+    )
+  })
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {directorDiv}
+  </div>)
 }
 
 export default Directors;
